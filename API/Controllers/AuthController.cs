b6daeb8fdef3b80144f3b117722c6e4cff2940cd
apriori-yarik <yarik2803chess@yandex.ru>
2022-03-17 20:08:36 +0200

    Auth controller added

---
+++ API/Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using Business.Dtos;
+using Business.Dtos.User;
+using Business.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("api/auth/register")]
+        public async Task<IActionResult> RegisterAsync(UserDto userDto)
+        {
+            await _authService.RegisterAsync(userDto);
+
+            return NoContent();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("api/auth/login")]
+        public async Task<IActionResult> LoginAsync(AuthDto authDto)
+        {
+            var tokenDto = await _authService.LoginAsync(authDto.Email, authDto.Password);
+
+            if (tokenDto == null)
+            {
+                return Unauthorized();
+            }
+
+            return Ok(tokenDto);
+        }
+    }
+}
