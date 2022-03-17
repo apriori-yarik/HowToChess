66d161142a896b614edba7f6ea83ec08ab199e93
apriori-yarik <yarik2803chess@yandex.ru>
2022-03-17 18:08:13 +0200

    User repo and service added

---
+++ Business/Services/UserService.cs
@@ -0,0 +1,48 @@
+﻿using Business.Dtos;
+using Business.Services.Interfaces;
+using DataAccess.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _repository;
+
+        public UserService(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task CreateAsync(UserDto userDto)
+        {
+            await _repository.CreateAsync(userDto);
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            await _repository.DeleteAsync(id);
+        }
+
+        public async Task<List<UserDtoWithId>> GetAllAsync()
+        {
+            var users = await _repository.GetAllAsync<UserDtoWithId>();
+            return users;
+        }
+
+        public async Task<UserDtoWithId> GetAsync(Guid id)
+        {
+            var user = await _repository.GetByIdAsync<UserDtoWithId>(id);
+            return user;
+        }
+
+        public async Task UpdateAsync(UserDtoWithId userDto)
+        {
+            await _repository.UpdateAsync(userDto);
+        }
+    }
+}
---
+++ DataAccess/Repositories/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using DataAccess.Entities;
+using DataAccess.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories
+{
+    public class UserRepository : BaseRepository<User>, IUserRepository 
+    {
+        public UserRepository(HowToChessDbContext context, IMapper mapper) : base(context, mapper)
+        {
+        }
+    }
+}
